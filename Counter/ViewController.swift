7d763f72e901de4f78e5f9228b555929775c9d10
Trashcor <trashmencor@inbox.ru>
2025-10-16 18:21:33 +0800

    fixed_app

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,26 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 0
+    private var count = 0
     
-    var dateFormat = DateFormatter()
+    private var dateFormat = DateFormatter()
     
-    @IBOutlet weak var counter: UILabel!
+    @IBOutlet private weak var counter: UILabel!
+        
+    @IBOutlet private weak var plusOne: UIButton!
     
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet private weak var minusOne: UIButton!
     
-    @IBOutlet weak var plusOne: UIButton!
-    
-    @IBOutlet weak var minusOne: UIButton!
-    
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
     
     
-    @IBOutlet weak var changeHistory: UITextView!
+    @IBOutlet private weak var changeHistory: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         plusOne.backgroundColor = .white
         minusOne.backgroundColor = .white
         plusOne.tintColor = .red
@@ -39,21 +37,15 @@ class ViewController: UIViewController {
         
         dateFormat.dateFormat = "dd.MM.yy HH:mm"
         
-        
-    }
-
-    @IBAction func clickOnButton(_ sender: Any) {
-        count += 1
-        counter.text = "Значение счетчика: \(count)"
     }
     
-    @IBAction func clickOnPlus(_ sender: Any) {
+    @IBAction private func clickOnPlus(_ sender: Any) {
         count += 1
         counter.text = "Значение счетчика: \(count)"
         changeHistoryFunction("значение изменено на +1")
     }
     
-    @IBAction func clickOnMinus(_ sender: Any) {
+    @IBAction private func clickOnMinus(_ sender: Any) {
         if count != 0 {
             count -= 1; changeHistoryFunction("значение изменено на -1")
         } else {
@@ -62,15 +54,15 @@ class ViewController: UIViewController {
         counter.text = "Значение счетчика: \(count)"
     }
     
-    @IBAction func reset(_ sender: Any) {
+    @IBAction private func reset(_ sender: Any) {
         count = 0
         counter.text = "Значение счетчика: \(count)"
         changeHistoryFunction("значение сброшено")
     }
     
-    func changeHistoryFunction(_ note: String) {
+    private func changeHistoryFunction(_ note: String) {
         let time = dateFormat.string(from: Date())
-        changeHistory.text = "[\(time)]:\(note)"
+        changeHistory.text = changeHistory.text + "[\(time)]:\(note)"
     }
     
 }
